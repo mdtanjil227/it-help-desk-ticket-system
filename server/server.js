091d106ee953fd7d1d166ef1a673c4d4b8b4d5c2
mdtanjil227 <tanjil227@gmail.com>
2026-03-03 20:01:20 +0000

    server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const pool = require("./db");
+
+pool.connect()
+    .then(() => console.log("Connected to PostgreSQL"))
+    .catch(err => console.error("DB connection error", err));
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("IT Helpdesk API Running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen (PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
