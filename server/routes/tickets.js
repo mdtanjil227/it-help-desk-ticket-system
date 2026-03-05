6e08c4f9a7a1422fe18b8e86744a3791f0481347
mdtanjil227 <tanjil227@gmail.com>
2026-03-04 20:50:57 -0700

    Ticket

--- server/routes/tickets.js
+++ server/routes/tickets.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../db");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "Tickets route working"
-  });
+router.post("/", authMiddleware, async (req, res) => {
+  try {
+    const { title, description, priority } = req.body;
+
+    const newTicket = await pool.query(
+      `INSERT INTO tickets (title, description, priority, user_id)
+       VALUES ($1, $2, $3, $4)
+       RETURNING *`,
+      [title, description, priority, req.user.userId]
+    );
+
+    res.status(201).json(newTicket.rows[0]);
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+
+    const tickets = await pool.query(
+      `SELECT * FROM tickets
+       WHERE user_id = $1
+       ORDER BY created_at DESC`,
+      [req.user.userId]
+    );
+
+    res.json(tickets.rows);
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 module.exports = router;
